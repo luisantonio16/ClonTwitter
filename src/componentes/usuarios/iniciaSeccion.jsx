72eda86a1a8f86fca9e881011585fad451cf2fda
luisantonio16 <luis16.16m@gmail.com>
2023-06-12 15:20:37 -0400

    update

--- src/componentes/usuarios/iniciaSeccion.jsx
+++ src/componentes/usuarios/iniciaSeccion.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
 function iniciarSeccion () {
   return (
     <>
-     <section className="login">
-          <div className="login-container">
-               <h2 className="login-title">Login</h2>
-              <form className="login-form">
-                <div className="login-text">
-                    <input type="text" placeholder="Usuario" />
+    <section className="Registrarse">
+          <div className="Registrarse-container">
+            <div className="Cabeza">
+                <h2 className="Registrarse-title">Hodiee</h2>
+            </div>
+              <form className="Registrarse-form">
+                <h2 className="Registrarse-title">Login</h2>
+                <div className="Registrarse-text">
+                    <input type="text" placeholder="usuario o correo electronico" name="nombre" />
+                </div>
 
+                <div className="Registrarse-text">
+                    <input type="password" placeholder="Contraseña" name="contraseña"  />
                 </div>
-                <div className="login-text">
-                    <input type="text" placeholder="Contraseña" />
-                </div>
-                <div className="login-text">
+                <div className="Registrarse-text">
                     <input type="submit" value="Enviar" />
                 </div>
               </form>
+              <div className="cuenta">
+                  <p className="Registrarse-text">Ya tienes una cuenta.?</p>
+                  <NavLink className="btn-ir" to="/registrarse">Inicia Seccion</NavLink>
+              </div>
           </div>
      </section>
     </>
--- src/componentes/usuarios/registrarse.jsx
+++ src/componentes/usuarios/registrarse.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 
 function registrarse () {
   return (
@@ -12,16 +12,29 @@ function registrarse () {
               <form className="Registrarse-form">
                 <h2 className="Registrarse-title">Registrarse</h2>
                 <div className="Registrarse-text">
-                    <input type="text" placeholder="Usuario" />
-
+                    <input type="text" placeholder="Nombre" name="nombre" />
                 </div>
                 <div className="Registrarse-text">
-                    <input type="text" placeholder="Contraseña" />
+                    <input type="text" placeholder="Apellido" name="apellido"  />
+                </div>
+                <div className="Registrarse-text">
+                    <input type="text" placeholder="Usuario" name="usuario"  />
+                </div>
+                <div className="Registrarse-text">
+                    <input type="text" placeholder="Email" name="email"  />
+                </div>
+                <div className="Registrarse-text">
+                    <input type="password" placeholder="Contraseña" name="contraseña"  />
                 </div>
                 <div className="Registrarse-text">
                     <input type="submit" value="Enviar" />
                 </div>
               </form>
+              <div className="cuenta">
+                  <p className="Registrarse-text">Ya tienes una cuenta.?</p>
+                  <NavLink className="btn-ir" to="/login">Inicia Seccion</NavLink>
+
+              </div>
           </div>
      </section>
     </>
